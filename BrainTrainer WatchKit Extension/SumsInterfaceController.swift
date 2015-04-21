cbbb1fa4af6ee963c1ee8d10e523cf8d72215650
Rommel Rico <rommeltj@gmail.com>
2015-04-21 08:32:33 -0700

    Initial UI and Interface Controllers

--- BrainTrainer WatchKit Extension/SumsInterfaceController.swift
+++ BrainTrainer WatchKit Extension/SumsInterfaceController.swift
@@ -1,6 +1,6 @@
 //
-//  InterfaceController.swift
-//  BrainTrainer WatchKit Extension
+//  SumsInterfaceController.swift
+//  BrainTrainer
 //
 //  Created by Rommel Rico on 4/21/15.
 //  Copyright (c) 2015 Rommel Rico. All rights reserved.
@@ -10,32 +10,31 @@ import WatchKit
 import Foundation
 
 
-class InterfaceController: WKInterfaceController {
+class SumsInterfaceController: WKInterfaceController {
 
-    @IBAction func plus() {
-        segue("plus")
+    @IBOutlet weak var resultsLabel: WKInterfaceLabel!
+    @IBOutlet weak var timer: WKInterfaceTimer!
+    @IBOutlet weak var sumLabel: WKInterfaceLabel!
+    
+    @IBAction func answer1() {
+        //TODO
     }
     
-    @IBAction func minus() {
-        segue("minus")
+    @IBAction func answer2() {
+        //TODO
     }
     
-    @IBAction func multiply() {
-        segue("multiply")
+    @IBAction func answer3() {
+        //TODO
     }
     
-    @IBAction func divide() {
-        segue("divide")
-    }
-    
-    func segue(type:String) {
-        pushControllerWithName("SumsInterfaceController", context: type)
+    @IBAction func answer4() {
+        //TODO
     }
     
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        
-        // Configure interface objects here.
+        println(context)
     }
 
     override func willActivate() {
