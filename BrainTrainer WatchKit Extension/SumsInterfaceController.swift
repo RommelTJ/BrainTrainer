54822b1e48ff8818b758ceb6715e8de7e043db20
Rommel Rico <rommeltj@gmail.com>
2015-04-21 08:57:17 -0700

    Generating random questions

--- BrainTrainer WatchKit Extension/SumsInterfaceController.swift
+++ BrainTrainer WatchKit Extension/SumsInterfaceController.swift
@@ -15,19 +15,30 @@ class SumsInterfaceController: WKInterfaceController {
     @IBOutlet weak var resultsLabel: WKInterfaceLabel!
     @IBOutlet weak var timer: WKInterfaceTimer!
     @IBOutlet weak var sumLabel: WKInterfaceLabel!
+    var firstOperand:Int = 0 //First operand
+    var secondOperand:Int = 0 //Second operand
+    var answer:Int = 0 //Answer
+    var sumCharacter = "" //+,-,x, or ÷
     
+    
+    @IBOutlet weak var answer1Label: WKInterfaceButton!
     @IBAction func answer1() {
         //TODO
     }
     
+    @IBOutlet weak var answer2Label: WKInterfaceButton!
     @IBAction func answer2() {
         //TODO
     }
     
+    
+    @IBOutlet weak var answer3Label: WKInterfaceButton!
     @IBAction func answer3() {
         //TODO
     }
     
+    
+    @IBOutlet weak var answer4Label: WKInterfaceButton!
     @IBAction func answer4() {
         //TODO
     }
@@ -46,14 +57,31 @@ class SumsInterfaceController: WKInterfaceController {
         var countdown = NSTimer.scheduledTimerWithTimeInterval(30, target: self, selector: Selector("quizComplete"), userInfo: nil, repeats: false)
         timer.start()
         if type == "plus" {
-            
+            firstOperand = Int(arc4random_uniform(11))
+            secondOperand = Int(arc4random_uniform(11))
+            answer = firstOperand + secondOperand
+            sumCharacter = "+"
         } else if type == "minus" {
-            
+            firstOperand = Int(arc4random_uniform(11))
+            secondOperand = Int(arc4random_uniform(11))
+            answer = firstOperand - secondOperand
+            sumCharacter = "-"
         } else if type == "multiply" {
-            
+            firstOperand = Int(arc4random_uniform(11))
+            secondOperand = Int(arc4random_uniform(11))
+            answer = firstOperand * secondOperand
+            sumCharacter = "x"
         } else if type == "divide" {
-            
+            firstOperand = Int(arc4random_uniform(11))
+            secondOperand = Int(arc4random_uniform(11))
+            if secondOperand == 0 {
+                //Cannot divide by 0
+                secondOperand = 1
+            }
+            answer = firstOperand / secondOperand
+            sumCharacter = "÷"
         }
+        sumLabel.setText("\(firstOperand) \(sumCharacter) \(secondOperand) =")
     }
     
     func quizComplete() {
